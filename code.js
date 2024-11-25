314c332b10f5713137def28226ae04c2892bc250
ESFLOWNK <otakuflownk@gmail.com>
2024-11-24 22:04:40 -0400

    New Update!

--- code.js
+++ code.js
@@ -1,4 +1,10 @@
 
+function keypressed(ev) {
+    if (ev.key === "Enter"){
+        check();
+    }
+}
+
 function setNumbers() {
     const m1 = document.getElementById("m1");
     const m2 = document.getElementById("m2");
@@ -6,7 +12,13 @@ function setNumbers() {
     m1.value = Math.floor(Math.random() * 11);
     m2.value = Math.floor(Math.random() * 11);
 
-    document.getElementById("result").value = 0;
+    if (document.getElementById("difConfig").innerText == "1"){
+        m1.value = Math.floor(Math.random() * 21);
+        m2.value = Math.floor(Math.random() * 21);
+    }
+
+    document.getElementById("result").value = null;
+    document.getElementById("result").focus();
 }
 
 function check(){
@@ -19,8 +31,23 @@ function check(){
         alert("Felicidades la has atinado!");
     }
     else{
-        alert("Has fallado :(");
+        alert("Has fallado :(\nLa respuesta era: "+String(m1.value * m2.value));
     }
 
     setNumbers();
+}
+
+// Doesnt work
+function toggleDif(){
+    const dif = document.getElementById("difConfig");
+    if (dif.innerText == "0" || dif.innerText == undefined) {
+        dif.innerText = "1";
+        document.getElementById("difBut").className = "difbut1";
+        document.getElementById("difBut").innerText = "Modo facil";
+    }
+    else if (dif.innerText == "1") {
+        dif.innerText = "0";
+        document.getElementById("difBut").className = "difbut0";
+        document.getElementById("difBut").innerText = "Modo dificil";
+    }
 }
